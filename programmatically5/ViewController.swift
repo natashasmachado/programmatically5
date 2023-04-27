50e10d5fe919654fc1e0c6cf8a39636d2789e4ec
Natasha Machado <natashamachado@Natashas-MacBook-Pro.local>
2023-04-27 12:36:39 -0700

    start of the project

---
+++ programmatically5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  programmatically5
+//
+//  Created by Natasha Machado on 2023-04-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
